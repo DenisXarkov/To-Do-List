6c860d7c08ad173b6b2da939dcc0a06613adfd92
DenisXarkov <denisxarkov@yandex.ru>
2022-05-10 14:42:48 +0300

    completed script, application logic is configured

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-let arr = [];// массив с тасками
+let arr = [];
 let toDo = document.querySelector('input');
 let button = document.querySelector('button');
 let inputToDoList = '';
+let sortConteiner = document.querySelector('.conteiner-sorting');
 let sortButton = document.querySelector('.sorting');
-sortButton.addEventListener('click', sortingToDo);
+let sortUp = document.querySelector('.sorting__down');
+
+
+// if (sortButton.style.display === 'block'){
+// 	sortButton.style.display = 'none';
+// 	sortUp.style.display = 'block';
+// } else {
+// 	sortButton.style.display = 'block';
+// 	sortUp.style.display = 'none';
+// }
+
+
+sortConteiner.addEventListener('click', sortingToDo);
 button.addEventListener('click', clickButton);
 toDo.addEventListener('input', inputToDo);
 document.addEventListener('keydown', func);
-let taskToDo= document.querySelector('.task-to-do');
+let taskToDo = document.querySelector('.task-to-do');
 let upSorted = false;
 
 function func(e) {
@@ -31,7 +44,6 @@ function clickButton() {
 	}
 
 
-	// let taskToDo = document.querySelector('.task-to-do');
 	let newElement = document.createElement('div');
 	taskToDo.append(newElement);
 	newElement.innerText = inputToDoList;
@@ -39,7 +51,6 @@ function clickButton() {
 	classListNewElement.add('toDoElement');
 	let elementDelete = document.createElement('div');
 	newElement.append(elementDelete);
-	// elementDelete.innerText = '';
 	let classListDelete = elementDelete.classList;
 	classListDelete.add('deleteTodo');
 	inputToDoList = '';
@@ -49,47 +60,33 @@ function clickButton() {
 	console.log(arr);
 }
 
-// let toDoDelete = document.querySelector('.task-to-do');
-// toDoDelete.addEventListener('click', begin);
-// function begin(el){
-// 	let parentElement= toDoDelete.parentElement;
-// 	toDoDelete.remove();
-// 	console.log(parentElement)
 
-// }
 
 
 function clickDeleteButton() {
-	// находим ближайший к кликнутой кнопке элемент - его надо будет удалить
 	let elementDiv = this.closest(".toDoElement");
 	elementDiv.remove();
 	let str = elementDiv.innerText;
-	//по внутреннему тексту этого элемента находим его индекс в массиве
 	let elementRemove = arr.indexOf(str);
-	// по найденному индексу удаляем элемент в массиве
 	arr.splice(elementRemove, 1);
-	// проверяем, если в блоке всех задач не осталось задач, то делаем весь блок невидимым
 	taskToDo.innerText == ""
 		? (taskToDo.style.display = "none")
 		: (taskToDo.style.display = "block");
 }
 
 function sortingToDo() {
-	taskToDo.innerText == '' ? null : sortButton.style.backgroundImage = 'url(/images/sort_3.svg)';
-	// taskToDo.innerText == '' ? null : upSorted = false ? 
-	// sortButton.style.background ='url(/images/sort_3.svg)':
-	// sortButton.style.background ='url(/images/sotr_2.svg)';
 
-	let sortArray = [...arr].sort((a,b)=>{
-		if(upSorted){
-			return (a < b) ? 1 
-			: (a > b) ? -1 : 0;
+	let sortArray = [...arr].sort((a, b) => {
+		if (upSorted) {
+			return (a < b) ? 1
+				: (a > b) ? -1 : 0;
+
 		}
-		return (a < b) ? -1 
-		: (a > b) ? 1 : 0;
+		return (a < b) ? -1
+			: (a > b) ? 1 : 0;
 	});
-	taskToDo.innerText='';
-	sortArray.forEach((el)=>{
+	taskToDo.innerText = '';
+	sortArray.forEach((el) => {
 		let newElement = document.createElement('div');
 		taskToDo.append(newElement);
 		newElement.innerText = el;
@@ -101,8 +98,19 @@ function sortingToDo() {
 		let classListDelete = elementDelete.classList;
 		classListDelete.add('deleteTodo');
 		elementDelete.addEventListener("click", clickDeleteButton);
+
 	})
+
+	if (sortButton.style.display === 'block') {
+		sortButton.style.display = 'none';
+		sortUp.style.display = 'block';
+	} else {
+		sortButton.style.display = 'block';
+		sortUp.style.display = 'none';
+	}
+
 	upSorted = !upSorted;
+
 	console.log(sortArray);
 }
 
